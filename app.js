d51e413041a6ecb3eee2ab43474826daf8ec8a91
Patrick <patr983e@stud.kea.dk>
2020-05-11 22:42:39 +0200

    navbar created

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  sideMenu();
+}
+
+function sideMenu() {
+  const hamburguer = document.querySelector(".hamburguerIcon");
+
+  hamburguer.addEventListener("click", changeStatus);
+}
+
+function changeStatus(evt) {
+  console.log(evt.target);
+  const menuContainer = document.querySelector(".navMenu");
+  console.log("left is " + menuContainer.style.left);
+  menuContainer.style.left == "0vw" ? (menuContainer.style.left = "100vw") : (menuContainer.style.left = "0vw");
+  console.log("left is " + menuContainer.style.left);
+
+  //change to cross
+  const topBar = document.querySelector(".ham_top");
+  const bottomBar = document.querySelector(".ham_bottom");
+  const hamburger = document.querySelector(".hamburguerIcon");
+
+  hamburger.style.display = "block";
+  topBar.style.transform = "rotate(-45deg)";
+  bottomBar.style.transform = "rotate(45deg)";
+}
