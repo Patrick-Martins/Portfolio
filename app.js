9d98883a3d9c7f8541099fab63bb0da056e5aa55
Patrick <patr983e@stud.kea.dk>
2020-05-16 19:52:09 +0200

    created functionality for opening a project

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   sideMenu();
+  projects();
 }
 
 function sideMenu() {
@@ -16,7 +17,7 @@ function changeStatus(evt) {
   console.log(evt.target);
   const menuContainer = document.querySelector(".navMenu");
   console.log("left is " + menuContainer.style.left);
-  menuContainer.style.left == "0vw" ? (menuContainer.style.left = "100vw") : (menuContainer.style.left = "0vw");
+  menuContainer.style.left == "0vw" ? ((menuContainer.style.left = "100vw"), (menuContainer.style.borderRadius = "50%")) : ((menuContainer.style.left = "0vw"), (menuContainer.style.borderRadius = "0"));
   console.log("left is " + menuContainer.style.left);
 
   //change to cross
@@ -27,4 +28,62 @@ function changeStatus(evt) {
   hamburger.style.display = "block";
   topBar.style.transform = "rotate(-45deg)";
   bottomBar.style.transform = "rotate(45deg)";
+}
+
+function projects() {
+  // document.querySelector(".description-container").addEventListener("click", expand);
+  document.querySelectorAll(".project-container").forEach((project) => {
+    console.log(project.id);
+    project.querySelector(".description-container").addEventListener("click", () => {
+      expand();
+    });
+  });
+}
+function expand() {
+  //REMOVE PREV CLASSES
+
+  document.querySelector(".project-container").classList.remove("closeAnimation");
+  document.querySelector(".hidden_content").classList.remove("closeTextAnimation");
+  document.querySelector(".description-container").classList.remove("showDescription");
+
+  const desc = document.querySelector(".description-container");
+  const project = document.querySelector(".project-container");
+  const content = document.querySelector(".hidden_content");
+
+  // project.style.width = "100%";
+  // project.style.height = "calc(100vh - var(--navHeight) )";
+  // project.style.padding = "10px";
+  // project.style.overflow = "scroll";
+
+  // project.style.top = "var(--navHeight)";
+
+  // project.style.borderRadius = "0";
+  // project.style.margin = "0 auto";
+
+  project.classList.add("projectAnimation");
+
+  // desc.style.opacity = "0";
+  desc.classList.add("hideDescription");
+
+  //scroll to that container id and then expand the text
+
+  content.classList.add("textAnimation");
+
+  //CLOSE THE PROJECT
+  content.addEventListener("click", closeProject);
+
+  //STOP SCROLL IN HTML
+  document.querySelector("html").style.overflow = "hidden";
+}
+
+function closeProject() {
+  document.querySelector(".project-container").classList.remove("projectAnimation");
+  document.querySelector(".project-container").classList.add("closeAnimation");
+  document.querySelector(".hidden_content").classList.remove("textAnimation");
+  document.querySelector(".hidden_content").classList.add("closeTextAnimation");
+  document.querySelector(".description-container").classList.remove("hideDescription");
+  document.querySelector(".description-container").classList.add("showDescription");
+
+  //LET SCROLL IN HTML
+  document.querySelector("html").style.overflow = "auto";
 }
